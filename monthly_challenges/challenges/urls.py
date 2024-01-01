c7e029703f591f696a716b3ede5ae1cc198b036e
f-ssemwanga <dssales@gmx.com>
2024-01-01 16:59:40 +0000

    created views and configured URLs

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+"""
+1. Perform a URL config following the steps below
+- create a that has has all supported urls and a view function to be triggered using the path() function
+- the path() function takes a string describing the url and a pointer to the view function
+- the views.py file must be imported here to access the views functions
+2. Connect the challenges URL config to the app wide /main global config urls file
+"""
+
+urlpatterns = [
+    # list of all supported urls
+    path("january", views.index),
+    path("february", views.feb),
+]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    """
+    - accepts  request parameter which is passed automatically via django
+    - returns an HttpResponse() object instance imported from django - see line 2
+    - pass the response data to the HttpResponse() instance which could be a string or an html file
+    """
+    return HttpResponse("Jan Works!")
+
+
+def feb(request):
+    return HttpResponse("Feb Now work!")
