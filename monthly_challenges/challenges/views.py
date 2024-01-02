472b19d0a314161c64f8e5dff36be7c52d1513b9
f-ssemwanga <dssales@gmx.com>
2024-01-02 11:34:21 +0000

    added a request parameter to the render function

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -52,7 +52,7 @@ def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
         # returning some form of HTML
-        return render("challenges/challenge.html")
+        return render(request, "challenges/challenge.html")
     except:
         return HttpResponseNotFound(f"<h1>This month is not supported!</h1>")
 
