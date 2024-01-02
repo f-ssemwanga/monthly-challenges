8ef485175958a69a17ca9d596ac1cd15a0fa8b20
f-ssemwanga <dssales@gmx.com>
2024-01-02 11:58:23 +0000

    Adding variables into templates with Interpolation

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -52,7 +52,12 @@ def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
         # returning some form of HTML
-        return render(request, "challenges/challenge.html")
+
+        return render(
+            request,
+            "challenges/challenge.html",
+            {"text": challenge_text, "month_name": month.capitalize()},
+        )
     except:
         return HttpResponseNotFound(f"<h1>This month is not supported!</h1>")
 
